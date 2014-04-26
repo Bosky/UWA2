03e8d40e60d4d6f777fdb82ee8aad8dc3fc9ac92
Bosky <olli.paasikoski@gmail.com>
2014-04-26 21:06:53 +0100

    again

---
+++ UWA.Core/BusinessLayer/Contracts/Person.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace UWA.Core.BusinessLayer.Contracts
+{
+    public class Person : IBusinessEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Office { get; set; }
+        public string Address { get; set; }
+    }
+}

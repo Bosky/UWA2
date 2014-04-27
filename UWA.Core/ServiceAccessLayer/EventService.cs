08ce9d63f8e37e828cb85d67ed7104fa396057ce
Bosky <olli.paasikoski@gmail.com>
2014-04-27 18:13:04 +0100

    eventService working

---
+++ UWA.Core/ServiceAccessLayer/EventService.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics;
+using RestSharp;
+
+namespace UWA.Core.ServiceAccessLayer
+{
+    public static class EventService
+    {
+        public const string FeedUrl = "http://olli.worcestercomputing.com/rss/eventfeed.rss";
+
+        private static RestClient _client;
+
+        //static EventService()
+        //{
+        //    _client = new RestClient
+        //    {
+        //        BaseUrl = FeedUrl
+        //    };
+        //}
+
+        public static EventFeed GetFeed()
+        {
+            _client = new RestClient {BaseUrl = FeedUrl};
+
+            // var request = new RestRequest {RequestFormat = DataFormat.Xml };
+
+            //var response = _client.Execute<EventItems>(request);
+
+            //Debug.Write(response.Data.Count.ToString());
+            //return response.Data;
+
+            var request = new RestRequest {RequestFormat = DataFormat.Xml};
+            _client.ExecuteAsync(request, response => Debug.WriteLine(response.Content));
+
+            var response2 = _client.Execute<EventFeed>(request);
+            Debug.Write(response2.Data.channel.item.Count.ToString());
+            return response2.Data;
+        }
+    }
+}
---
+++ UWA.Core/ServiceAccessLayer/RSSModel.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections.Generic;
+
+namespace UWA.Core.ServiceAccessLayer
+{
+    /// <summary>
+    ///  RSSModel Contains a set of POCO objects that
+    ///  functions as Data Transfer Objects (DTOs).
+    /// </summary>
+    ///   
+    public class EventFeed
+    {
+        public string version { get; set; }
+        public EventChannel channel { get; set; }
+    }
+
+    public class EventChannel
+    {
+        public string title { get; set; }
+        public string link { get; set; }
+        public string description { get; set; }
+        //public image Image { get; set; }
+        public EventItems item { get; set; }
+    }
+
+    //public class image
+    //{
+    //    public string Title { get; set; }
+    //    public string Url { get; set; }
+    //    public string Link { get; set; }
+    //    public string Width { get; set; }
+    //    public string Height { get; set; }
+    //}
+
+    public class EventItems : List<item> { }
+
+    public class item
+    {
+        public string title { get; set; }
+        public string pubDate { get; set; }
+        public string link { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }              
+    }
+
+}
